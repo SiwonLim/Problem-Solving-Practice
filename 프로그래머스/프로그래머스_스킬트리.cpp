9ab655f94959768b64e5f52ed649bc8f1fb28a62
Siwon Lim(Beth) <51152816+SiwonLim@users.noreply.github.com>
2020-04-16 17:13:17 +0900

    Update 프로그래머스_스킬트리.cpp

--- 프로그래머스/프로그래머스_스킬트리.cpp
+++ 프로그래머스/프로그래머스_스킬트리.cpp
@@ -5,31 +5,32 @@
 
 using namespace std;
 map<char, int> m;
+vector<int> origin;
 
 int solution(string skill, vector<string> skill_trees) {
 	int answer = 0;
 	int s1 = skill.length(), s2 = skill_trees.size();
 	for (int i = 0; i < s1; i++) {
-		m.insert({skill[i],i+1});
+		m.insert({ skill[i],i + 1 });
+		origin.push_back(i + 1);
 	}
+
 	for (int i = 0; i < s2; i++) {
 		string str = skill_trees[i];
-		string dummy = "";
-		bool notMatch = false;
-		vector<int> check;
+		bool check = true;
+		vector<int> tmp;
 		for (int j = 0; j < str.length(); j++) {
-			if(m[str[j]] != 0)
-				check.push_back(m[str[j]]);
+			if (m[str[j]] != 0)
+				tmp.push_back(m[str[j]]);
 		}
-		
-		for (int j = 1; j < check.size(); j++) {
-			if (check[j-1] > check[j]) {
-				notMatch = true;
+
+		for (int j = 0; j < tmp.size(); j++) {
+			if (origin[j] != tmp[j]) {
+				check = false;
 				break;
 			}
 		}
-		if (!check.empty() && check.back() != check.size()) notMatch = true;
-		if (!notMatch) answer++;
+		if (check) answer++;
 	}
 	return answer;
 }
