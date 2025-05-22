ca37ed88629d403a567c47f958a008a73ed17254
siwonlim <dbu7422@gmail.com>
2025-05-21 23:47:12 -0700

    2025-05-21

--- 2025 Practice/2025-prac.py
+++ 2025 Practice/2025-prac.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         temp = 1
         count = 1
-
         for i in range(1, len(nums)):
             if nums[i] == nums[i - 1]:
                 count += 1
@@ -16,9 +15,8 @@ class Solution(object):
             if count <= 2:
                 nums[temp] = nums[i]
                 temp += 1
-
         return temp
         
 sol = Solution()
-result = sol.removeDuplicates([1,1,1,2,2,3])
+result = sol.removeDuplicates([1,1,1,1,1,2,2,3])
 print(result)
